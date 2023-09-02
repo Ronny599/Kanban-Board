01db9d9a79bd8fd823c936480dc195845b7bdb55
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-02 21:39:32 +0530

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
-
+import Navbar from './components/Navbar';
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
-  return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <div>
+    <>
+      <Navbar/>
+    </>
+  </div>;
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+    	<nav className="navbar bg-dark border-bottom border-body">
+        <div className="container-fluid justify-content-center">
+          <span className="navbar-brand mb-0 h1" style="font-size:30px; color:white;">Kanban Board</span>
+        </div>
+        <form className="container-fluid justify-content-start">
+          {/* <button class="btn btn-outline-success me-2" type="button">Main button</button> */}
+          <button className="btn btn-outline-success" type="button"   onclick="openForm()">Add Task</button>
+        </form>
+      </nav>
+    </>
+  );
+}
