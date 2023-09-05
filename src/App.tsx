9d6e88b207626bbe4965c5b1ef31a1a3c360ce94
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-05 13:11:45 +0530

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
+import Footer from './components/Footer';
+
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
       <>
         <Navbar />
         <Content />
+        <Footer />
       </>
     </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -9,78 +9,92 @@ export default function Content() {
   };
   return (
     <>
-      <h6 style="padding:5px;">kumarronit599@gmail.com</h6>
-      <div className="container" style={{backgroundColor: '#F76C6C'}}>
-        <div className="row">
-          <div className="col" style={{border: '3px solid #f1f1f1' }}>
-            <h3 style="text-align: center;">To Do</h3>
-            <div
-              className="card"
-              style={{width: '18rem', backgroundColor: '#24305E', color: 'white',}}
-            >
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="card-link">
-                  Details
-                </a>
-                <a href="#" className="card-link">
-                  Edit
-                </a>
-                <a href="#" className="card-link">
-                  Delete
-                </a>
+      <h6 style={{ padding: '5px' }}>kumarronit599@gmail.com</h6>
+      <div className="text-bg-light p-3">
+        <div className="container" style={{ backgroundColor: '#F76C6C' }}>
+          <div className="row">
+            <div className="col" style={{ border: '3px solid #f1f1f1' }}>
+              <h3 style={{ textAlign: 'center' }}>To Do</h3>
+              <div
+                className="card"
+                style={{
+                  width: '18rem',
+                  backgroundColor: '#24305E',
+                  color: 'white',
+                }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="card-link">
+                    Details
+                  </a>
+                  <a href="#" className="card-link">
+                    Edit
+                  </a>
+                  <a href="#" className="card-link">
+                    Delete
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col" style={{border: '3px solid #f1f1f1'}}>
-            <h3 style={{textAlign: 'center'}}>Doing</h3>
-            <div
-              className="card"
-              style={{width: '18rem', backgroundColor: '#24305E', color: 'white'}}
-            >
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="card-link">
-                  Details
-                </a>
-                <a href="#" className="card-link">
-                  Edit
-                </a>
-                <a href="#" className="card-link">
-                  Delete
-                </a>
+            <div className="col" style={{ border: '3px solid #f1f1f1' }}>
+              <h3 style={{ textAlign: 'center' }}>Doing</h3>
+              <div
+                className="card"
+                style={{
+                  width: '18rem',
+                  backgroundColor: '#24305E',
+                  color: 'white',
+                }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="card-link">
+                    Details
+                  </a>
+                  <a href="#" className="card-link">
+                    Edit
+                  </a>
+                  <a href="#" className="card-link">
+                    Delete
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col" style={{border: '3px solid #f1f1f1'}}>
-            <h3 style={{textAlign: 'center'}}>Done</h3>
-            <div
-              className="card"
-              style={{width: '18rem', backgroundColor: '#24305E', color: 'white'}}
-            >
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="card-link">
-                  Details
-                </a>
-                <a href="#" className="card-link">
-                  Edit
-                </a>
-                <a href="#" className="card-link">
-                  Delete
-                </a>
+            <div className="col" style={{ border: '3px solid #f1f1f1' }}>
+              <h3 style={{ textAlign: 'center' }}>Done</h3>
+              <div
+                className="card"
+                style={{
+                  width: '18rem',
+                  backgroundColor: '#24305E',
+                  color: 'white',
+                }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="card-link">
+                    Details
+                  </a>
+                  <a href="#" className="card-link">
+                    Edit
+                  </a>
+                  <a href="#" className="card-link">
+                    Delete
+                  </a>
+                </div>
               </div>
             </div>
           </div>
