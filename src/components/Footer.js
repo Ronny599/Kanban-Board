d5cc7a38dd6ebdbbfa10375e90fb2f8e83c9900f
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-05 13:18:43 +0530

    1 file modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,11 +19,11 @@ export default function Footer() {
     </a>
 
     <ul className="nav col-md-4 justify-content-end">
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
+      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Features</a></li>
+      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">About</a></li>
     </ul>
   </footer>
     </div>
