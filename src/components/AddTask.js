bb196177e109254e0c4d3106a2fb7a3d84cde2eb
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-09-07 19:35:46 +0530

    1 file modified, 1 added

---
+++ src/components/AddTask.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import "./style.css";
+
+export default function Navbar() {
+
+  const openForm= ()=>{
+    // document.getElementById('myForm').form-popup.style.display='block';
+  }
+  const closeForm= ()=>{
+    // document.getElementById('myForm').style.display='none';
+  }
+  return (
+    <>
+      <div className="style.form-popup" id="myForm">
+        <form action="AddTask.js" className="form-container">
+          <h2 style={{ textAlign: 'center'}}>Add Task</h2>
+          <label forHtml="title">
+            <b>Title</b>
+          </label>
+          <input type="text" placeholder="Enter Title" name="title" required />
+          <label forHtml="Description">
+            <b>Description</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Description"
+            name="detail"
+            required
+          />
+          <label forHtml="Validity">
+            <b>Valid Till</b>
+          </label>
+          &nbsp;&nbsp;
+          <input
+            type="date"
+            name="validity"
+            required
+          />
+          <button type="submit" className="btn">
+            Add
+          </button>
+          <button type="button" className="btn cancel" onclick={closeForm()}>
+            Close
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,6 @@ import * as React from 'react';
 import "./style.css";
 
 export default function Navbar() {
-
-  const openForm= ()=>{
-    document.getElementById('myForm').form-popup.style.display='block';
-  }
-  const closeForm= ()=>{
-    // document.getElementById('myForm').style.display='none';
-  }
   return (
     <>
       <nav className="navbar bg-dark border-bottom border-body">
@@ -21,7 +14,6 @@ export default function Navbar() {
           </span>
         </div>
         <form className="container-fluid justify-content-start">
-          {/* <button class="btn btn-outline-success me-2" type="button">Main button</button> */}
           <button
             className="btn btn-outline-success"
             type="button"
@@ -31,39 +23,6 @@ export default function Navbar() {
           </button>
         </form>
       </nav>
-      <div className="style.form-popup" id="myForm">
-        <form action="AddTask.js" className="form-container">
-          <h2 style={{ textAlign: 'center'}}>Add Task</h2>
-          <label forHtml="title">
-            <b>Title</b>
-          </label>
-          <input type="text" placeholder="Enter Title" name="title" required />
-          <label forHtml="Description">
-            <b>Description</b>
-          </label>
-          <input
-            type="password"
-            placeholder="Enter Description"
-            name="detail"
-            required
-          />
-          <label forHtml="Validity">
-            <b>Valid Till</b>
-          </label>
-          &nbsp;&nbsp;
-          <input
-            type="date"
-            name="validity"
-            required
-          />
-          <button type="submit" className="btn">
-            Add
-          </button>
-          <button type="button" className="btn cancel" onclick={closeForm()}>
-            Close
-          </button>
-        </form>
-      </div>
     </>
   );
 }
